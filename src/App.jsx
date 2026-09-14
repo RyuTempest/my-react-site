f9a16b88fa8b9c012bbaf5801e917735b0d08aa4
Ray Avila <88654645+RyuTempest@users.noreply.github.com>
2026-09-14 18:43:58 +0800

    Update email link to use mailto protocol and add Vercel configuration file

--- src/App.jsx
+++ src/App.jsx
@@ -155,7 +155,7 @@ function App() {
         </p>
 
         <div className="contact-links">
-          <a href="avilarayrafael8@gmail.com">
+          <a href="mailto:avilarayrafael8@gmail.com">
             Email
           </a>
 
